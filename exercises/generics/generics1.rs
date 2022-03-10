e43383f1d9fa6cfe6da484570afa9d1318feb36a
a-red-ant <cgr1963@gmail.com>
2022-03-10 21:45:53 +0000

    solving exercises

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -3,9 +3,8 @@
 
 // Execute `rustlings hint generics1` for hints!
 
-// I AM NOT DONE
-
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    // this is OK too let mut shopping_list = Vec::new();
+    let mut shopping_list : Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
